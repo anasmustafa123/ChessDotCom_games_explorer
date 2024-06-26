96522f5b7d62f4cf966a1ddac2ba09c2ef708bc3
Your Name <anasmuostafa@gmail.com>
2024-06-26 19:37:03 +0300

    adding vercel web analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { UserProvider } from "./contexts/UserStaticContext.jsx";
+import { Analytics } from "@vercel/analytics/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UserProvider>
       <App />
+      <Analytics />
     </UserProvider>
   </React.StrictMode>
 );
